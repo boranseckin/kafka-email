39d7a56f803f623c93cd94698224f437c9c2a11f
Boran Seckin <boran@apostonews.com>
2021-08-21 16:15:36 +0300

    Remove express

--- producer.js
+++ producer.js
@@ -1,15 +1,12 @@
 require('dotenv').config();
 
-const express = require('express');
-const app = express();
-
-app.use(require('body-parser').json());
-
 const kafka = require('./kafka');
 const producer = kafka.producer();
 
 (async () => {
     await producer.connect();
+
+    let i = 1;
 
     setInterval(async () => {
         try {
@@ -20,8 +17,8 @@ const producer = kafka.producer();
                     key: _id,
                     value: JSON.stringify({
                         _id,
-                        from: "hello@aposto.com",
-                        to: `${Math.random().toString(16).slice(10)}@example.com`,
+                        from: "team@aposto.com",
+                        to: `success+${i}@simulator.amazonses.com`,
                         subject: "Test email from Aposto!",
                         text: "Hello World",
                         html: "<b>Hello World<b>",
@@ -29,33 +26,12 @@ const producer = kafka.producer();
                 }],
             });
     
+            i += 1;
             console.log('Published message', { responses });
         } catch (error) {
             console.log('Error publishing message', error);
         }
-    }, 50);
-
-    app.post('/email', async (req, res) => {
-        try {
-            const responses = await producer.send({
-                topic: process.env.TOPIC,
-                messages: [{
-                    key: req.body._id,
-                    value: JSON.stringify(req.body),
-                }],
-            });
-
-            console.log('Published message', { responses });
-        } catch (error) {
-            console.log('Error publishing message', error);
-        }
-
-        res.sendStatus(200);
-    });
-
-    app.listen(process.env.PORT || 3000, () => {
-        console.log(`Server listening on port ${process.env.PORT || 3000}`);
-    });
+    }, 500);
 })().catch(async (error) => {
     console.error(error);
     await producer.disconnect();
